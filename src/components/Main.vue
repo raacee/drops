2ca4092eb52ac39f0c88d98c457a9bf479ed45d0
racel <raceldamechli@outlook.fr>
2023-02-12 00:17:52 +0200

    setting up development beginning

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default{
+  name:"MainApp",
+  data(){
+    return {};
+  },
+  components:{
+
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
+import MainApp from './components/Main.vue'
 import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(MainApp).mount('#app')
