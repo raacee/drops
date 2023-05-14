40e799971b1d91da6b6ed9a81d6655892a977b8f
racel <raceldamechli@outlook.fr>
2023-05-14 17:02:44 +0300

    added more green

--- src/components/Wave.vue
+++ src/components/Wave.vue
@@ -1,6 +1,7 @@
 <template>
     <div style="display: flex; align-items: center; justify-content: center;">
         <div class="ocean">
+
             <svg class="wave" viewBox="0 0 12960 1120" :style="'fill:rgb('+this.color+')'">
 
                 <path
@@ -11,10 +12,10 @@
                             repeatCount="indefinite"
                             attributeName="d"
                             values="
-                M9720,320C8100,320,8100,0,6480,0S4860,320,3240,320,1620,0,0,0V1120H12960V0C11340,0,11340,320,9720,320Z;
-                M9720,0C8100,0,8100,319,6480,319S4860,0,3240,0,1620,320,0,320v800H12960V320C11340,320,11340,0,9720,0Z;
-                M9720,320C8100,320,8100,0,6480,0S4860,320,3240,320,1620,0,0,0V1120H12960V0C11340,0,11340,320,9720,320Z
-              "/>
+                        M9720,320C8100,320,8100,0,6480,0S4860,320,3240,320,1620,0,0,0V1120H12960V0C11340,0,11340,320,9720,320Z;
+                        M9720,0C8100,0,8100,319,6480,319S4860,0,3240,0,1620,320,0,320v800H12960V320C11340,320,11340,0,9720,0Z;
+                        M9720,320C8100,320,8100,0,6480,0S4860,320,3240,320,1620,0,0,0V1120H12960V0C11340,0,11340,320,9720,320Z"
+                    />
 
                 </path>
 
@@ -34,7 +35,7 @@ export default {
             const redInitial = 204
             const redFinal = 79
 
-            const greenInitial = 102
+            const greenInitial = 150
             const greenFinal = 195
 
             const blueInitial = 0
